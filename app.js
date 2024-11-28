28a3bf153fcaefb768e6f9d29213d450f67b0861
shehanminura <shehanminuradp@gmail.com>
2024-11-28 16:39:22 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,427 @@
+let burgger = [
+    {
+        itemcode : "B1001",
+        itemname :"Classic Burger (Large)",
+        price : 750.00,
+        discount : 0
+    },{
+        itemcode : "B1002",
+        itemname :"Classic Burger (Regular) ",
+        price : 1500.00,
+        discount :15
+  
+    },{
+        itemcode : "B1003",
+        itemname :"Turkey Burger",
+        price : 1600.00,
+        discount : 0
+
+    },{
+        itemcode : "B1004",
+        itemname :"Chicken Burger (Large) ",
+        price : 1400.00,
+        discount : 0
+
+    },{
+        itemcode : "B1005",
+        itemname :"Chicken Burger (Regular)  ",
+        price : 800.00,
+        discount : 20
+
+    },{
+        itemcode : "B1006",
+        itemname :"Cheese Burger (Large) ",
+        price : 1000.00,
+        discount : 0
+
+    },{
+        itemcode : "B1007",
+        itemname :"Cheese Burger (Regular) ",
+        price : 600.00,
+        discount : 0
+
+    },{
+        itemcode : "B1008",
+        itemname :"Bacon Burger ",
+        price : 650.00,
+        discount : 15
+
+    },{
+        itemcode : "B1009",
+        itemname :"Shawarma Burger",
+        price : 800.00,
+        discount : 0
+
+    },{
+        itemcode : "B1010",
+        itemname :"Olive Burger ",
+        price : 1800.00,
+        discount : 0
+
+    },{
+        itemcode : "B1012",
+        itemname :"Double-Cheese Burger ",
+        price : 1250.00,
+        discount : 20
+
+    },{
+        itemcode : "B1013",
+        itemname :"Crispy Chicken Burger (Regular) ",
+        price : 1200.00,
+        discount : 0
+
+    },{
+        itemcode : "B1014",
+        itemname :"Crispy Chicken Burger (Regular) ",
+        price : 1600.00,
+        discount : 10
+
+    },{
+        itemcode : "B1015",
+        itemname :"Paneer Burger ",
+        price : 900.00,
+        discount :0
+}];
+
+let Chicken = [
+    {
+        itemcode : "B1038",
+        itemname :"Fried Chicken (Small)  ",
+        price : 1200.00,
+        discount : 0
+   
+    },{
+        itemcode : "B1039",
+        itemname :" Fried Chicken (Regular) ",
+        price : 2300.00,
+        discount : 10
+   
+    },{
+        itemcode : "B1040",
+        itemname :"Fried Chicken (Large)  ",
+        price : 3100.00,
+        discount : 5
+   
+    },{
+        itemcode : "B1041",
+        itemname :"Fried Chicken (Large)  ",
+        price : 2400.00,
+        discount : 0
+   
+    },{
+        itemcode : "B1042",
+        itemname :"Devilled Chicken (Large)  ",
+        price : 900.00,
+        discount : 0
+   
+    },{
+        itemcode : "B1043",
+        itemname :" BBQ Chicken (Regular) ",
+        price : 2100.00,
+        discount : 0
+   
+    }
+  ];
+let Beverages = [
+    {
+        itemcode : "B1044",
+        itemname :" Pepsi (330ml)  ",
+        price : 990.00,
+        discount : 5 
+    },{
+        
+        itemcode : "B1045",
+        itemname :" Coca-Cola (330ml)  ",
+        price : 1230.00,
+        discount : 0
+    },{
+        
+        itemcode : "B1046",
+        itemname :" Sprite (330ml)   ",
+        price : 1500.00,
+        discount : 3
+    },{
+        
+        itemcode : "B1047",
+        itemname :" Mirinda (330ml)  ",
+        price : 850.00,
+        discount : 7
+    }
+  ]
+
+let fries=[
+    {
+        itemcode:"B1025",
+        itemname:"Steak Fries (Large) ",
+        pricelkr:1200.00,
+        discount:0
+    },  {
+        itemcode:"B1026",
+        itemname:"Steak Fries (Medium)",
+        pricelkr:600.00,
+        discount:0
+    },  {
+        itemcode:"B1027",
+        itemname:"French Fries (Large)",
+        pricelkr:"800.00",
+        discount:0
+    },  {
+        itemcode:"B1028",
+        itemname:"French Fries (Medium)",
+        pricelkr:650.00,
+        discount:0
+    },  {
+        itemcode:"B1029",
+        itemname:"French Fries (Small)",
+        pricelkr:450.00,
+        discount:0
+    },  {
+        itemcode:"B1030",
+        itemname:"Sweet Potato Fries (Large)",
+        pricelkr:600.00,
+        discount:0
+    },
+
+ ];
+
+let pasta=[
+    {
+        itemcode:"B1031",
+        itemname:"Chicken n Cheese Pasta ",
+        pricelkr:1600.00,
+        discount:15
+    },  {
+        itemcode:"B1032",
+        itemname:"Chicken Penne Pasta ",
+        pricelkr:1700.00,
+        discount:0
+    },  {
+        itemcode:"B1033",
+        itemname:"Ground Turkey Pasta Bake",
+        pricelkr:2900.00,
+        discount:0
+    },  {
+        itemcode:"B1034",
+        itemname:"Creamy Shrimp Pasta ",
+        pricelkr:2000.00,
+        discount:0
+    },  {
+        itemcode:"B1035",
+        itemname:"Lemon Butter Pasta",
+        pricelkr:1950.00,
+        discount:0
+    },  {
+        itemcode:"B1036",
+        itemname:"Tagliatelle Pasta",
+        pricelkr:2400.00,
+        discount:0
+    },
+    {
+        itemcode:"B1037",
+        itemname:"Baked Ravioli",
+        pricelkr:2000.00,
+        discount:0
+    },
+
+ ];
+
+
+submarines=[
+    {
+      itemcode:"B1016",
+      itemame:"Crispy Chicken Submarine (Large) ",
+      price:2000.00,
+      discount:0, 
+    },{
+        itemcode:"B1017",
+        itemame:"Crispy Chicken Submarine (Regular)", 
+        price:1500.00,
+        discount:0,
+    },{
+        itemcode:"B1018" ,
+        itemame:"Chicken Submarine (Large)", 
+        price:1800.00 ,
+        discount:3 ,
+    },{
+        itemcode:"B1019", 
+        itemame:"Chicken Submarine (Regular)", 
+        price:1400.00 ,
+        discount:0, 
+    },{
+        itemcode:"B1020", 
+        itemame:"Grinder Submarine" ,
+        price:2300.00, 
+        discount:0,
+    },{
+        itemcode:"B1021", 
+        itemame:"Cheese Submarine", 
+        price:2200.00,
+        discount:0,
+    },{
+        itemcode:"B1022",
+        itemame:"Double Cheese n Chicken Submarine", 
+        price:1900.00,
+        discount:16,
+    },{
+        itemcode:"B1023", 
+        itemame:"Special Horgie Submarine",
+        price:2800.00,
+        discount:0,
+    },{
+        itemcode:"B1024",
+        itemame:"MOS Special Submarine",
+        price:3000.00,
+        discount:0,
+    }
+     ];
+
+     submarines.forEach(element => {
+        document.getElementById("submarines").innerHTML +=	
+                    `<div class="card m-3 " style="width: 18rem;">
+                    <img class="card-img-top" src="https://i.pinimg.com/736x/9a/77/a9/9a77a98489c7ee09deefd79d5fcfee79.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">${element.itemname}</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">${element.price}</li>
+                        <li class="list-group-item">${element.discount}%</li>
+                        <li class="list-group-item">Total : </li>
+                    </ul>
+                    <div class="card-body">
+                        <button class="btn bg-danger text-light" >Place Order</button>
+                          
+                        <a href="#" class="card-link">Add to Cart</a>
+                    </div>
+                    </div>`
+        
+    });
+
+
+
+
+Chicken.forEach(element => {
+	document.getElementById("Chicken").innerHTML +=	
+				`<div class="card m-3 " style="width: 18rem;">
+				<img class="card-img-top" src="img/burgerHomepage.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">${element.itemname}</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				</div>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item">${element.price}</li>
+					<li class="list-group-item">${element.discount}%</li>
+					<li class="list-group-item">Total : </li>
+				</ul>
+				<div class="card-body">
+					<button class="btn bg-danger text-light" >Place Order</button>
+                      
+					<a href="#" class="card-link">Add to Cart</a>
+				</div>
+				</div>`
+	
+});
+
+
+pasta.forEach(element => {
+	document.getElementById("pasta").innerHTML +=	
+	
+				`<div class="card m-3 " style="width: 18rem;">
+				<img class="card-img-top" src="img/burgerHomepage.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">${element.itemname}</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				</div>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item">${element.price}</li>
+					<li class="list-group-item">${element.discount}%</li>
+					<li class="list-group-item">Total : </li>
+				</ul>
+				<div class="card-body">
+					<button class="btn bg-danger text-light" >Place Order</button>
+                      
+					<a href="#" class="card-link">Add to Cart</a>
+				</div>
+				</div>`
+	
+});
+
+
+
+fries.forEach(element => {
+	document.getElementById("fries").innerHTML +=	
+	
+				`<div class="card m-3 " style="width: 18rem;">
+				<img class="card-img-top" src="img/burgerHomepage.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">${element.itemname}</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				</div>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item">${element.price}</li>
+					<li class="list-group-item">${element.discount}%</li>
+					<li class="list-group-item">Total : </li>
+				</ul>
+				<div class="card-body">
+					<button class="btn bg-danger text-light" >Place Order</button>
+                      
+					<a href="#" class="card-link">Add to Cart</a>
+				</div>
+				</div>`
+	
+});
+
+Beverages.forEach(element => {
+	document.getElementById("Beverages").innerHTML +=	
+	
+				`<div class="card m-3 " style="width: 18rem;">
+				<img class="card-img-top" src="https://blog.ncponline.com/wp-content/uploads/2023/07/Blog_Summer-Beverage-Trends-Feature-Image.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">${element.itemname}</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				</div>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item">${element.price}</li>
+					<li class="list-group-item">${element.discount}%</li>
+					<li class="list-group-item">Total : </li>
+				</ul>
+				<div class="card-body">
+					<button class="btn bg-danger text-light" >Place Order</button>
+                      
+					<a href="#" class="card-link">Add to Cart</a>
+				</div>
+				</div>`
+	
+});
+
+
+burgger.forEach(element => {
+	document.getElementById("burgers").innerHTML +=	
+	
+				`<div class="card m-3 " style="width: 18rem;">
+				<img class="card-img-top" src="https://freshchoicenelson.co.nz/wp-content/uploads/2019/06/free-range-chicken-nelson.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">${element.itemname}</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				</div>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item">${element.price}</li>
+					<li class="list-group-item">${element.discount}%</li>
+					<li class="list-group-item">Total : </li>
+				</ul>
+				<div class="card-body">
+					<button class="btn bg-danger text-light" >Place Order</button>
+                      
+					<a href="#" class="card-link">Add to Cart</a>
+				</div>
+				</div>`
+	
+});
+
+
+
+
+
+
+
+ 
